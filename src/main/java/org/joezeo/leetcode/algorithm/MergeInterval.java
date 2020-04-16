6d442dc22f5765d0b9ba95139dadcef6150213da
Joezeo <1252795700@qq.com>
2020-04-16 23:18:13 +0800

    leetcode:MergeInterval

---
+++ src/main/java/org/joezeo/leetcode/algorithm/MergeInterval.java
@@ -0,0 +1,53 @@
+package org.joezeo.leetcode.algorithm;
+
+/**
+ * @author Joezeo
+ * @date 2020/4/16 22:31
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给出一个区间的集合，请合并所有重叠的区间。
+ *
+ * 示例 1:
+ * 输入: [[1,3],[2,6],[8,10],[15,18]]
+ * 输出: [[1,6],[8,10],[15,18]]
+ * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ *
+ * 示例 2:
+ * 输入: [[1,4],[4,5]]
+ * 输出: [[1,5]]
+ * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-intervals
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MergeInterval {
+    public static void main(String[] args) {
+    }
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        if (intervals.length == 0 || intervals == null) return res.toArray(new int[0][]);
+        // 按左端点进行排序，对起点终点进行排序
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int i = 0;
+        while (i < intervals.length) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            // 如果有重叠，循环判断哪个起点满足条件
+            while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
+                i++;
+                right = Math.max(right, intervals[i][1]);
+            }
+            // 将现在的区间放进res里面
+            res.add(new int[]{left, right});
+            // 接着判断下一个区间
+            i++;
+        }
+        return res.toArray(new int[0][]);
+    }
+}
