a5a4ce1f0fc9c5d672dbf87c1d92d8149c660a8e
Joezeo <1252795700@qq.com>
2020-04-16 23:21:19 +0800

    leetcode:MergeInterval

--- src/main/java/org/joezeo/leetcode/algorithm/MergeInterval.java
+++ src/main/java/org/joezeo/leetcode/algorithm/MergeInterval.java
@@ -39,6 +39,7 @@ public class MergeInterval {
             int left = intervals[i][0];
             int right = intervals[i][1];
             // 如果有重叠，循环判断哪个起点满足条件
+            // 将左端点相同的数组进行两两合并
             while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
                 i++;
                 right = Math.max(right, intervals[i][1]);
