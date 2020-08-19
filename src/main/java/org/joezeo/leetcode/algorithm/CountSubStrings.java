ae1725a104dcc86cbd260aa7314224ae7aabd76f
zhaozhe <1252795700@qq.com>
2020-08-19 17:49:16 +0800

    dynamic programming learning.

---
+++ src/main/java/org/joezeo/leetcode/algorithm/CountSubStrings.java
@@ -0,0 +1,41 @@
+package org.joezeo.leetcode.algorithm;
+
+/**
+ * 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+ *
+ * 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入："abc"
+ * 输出：3
+ * 解释：三个回文子串: "a", "b", "c"
+ * 示例 2：
+ *
+ * 输入："aaa"
+ * 输出：6
+ * 解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindromic-substrings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ZhaoZhe
+ * @email joezane.cn@gmail.com
+ * @date 2020/8/19 16:18
+ */
+public class CountSubStrings {
+
+    private DynamicProgramming please_goto_this_class;
+
+    // 要使用动态规划
+    public int countSubstrings(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+
+        }
+        return count;
+    }
+}
---
+++ src/main/java/org/joezeo/leetcode/algorithm/DynamicProgramming.java
@@ -0,0 +1,71 @@
+package org.joezeo.leetcode.algorithm;
+
+/**
+ * 动态规划学习
+ *
+ * @author ZhaoZhe
+ * @email joezane.cn@gmail.com
+ * @date 2020/8/19 16:27
+ */
+public class DynamicProgramming {
+
+    public static void main(String[] args) {
+        fibonacci();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////                                              Fibonacci                                                    ////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void fibonacci() {
+        long startTime, endTime, fibo;
+        int times = 40;
+        // 递归非常花费时间
+        startTime = System.currentTimeMillis();
+        fibo = traditionalFibonacci(times);
+        endTime = System.currentTimeMillis();
+        System.out.println("递归计算 -> fibo=" + fibo + " -> 花费时间=" + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        fibo = fibonacciSequence(times);
+        endTime = System.currentTimeMillis();
+        System.out.println("动态规划 -> fibo=" + fibo + " -> 花费时间=" + (endTime - startTime));
+    }
+
+    /*
+        fn = fn-1 + fn-2
+        ...
+        f3 = f2 + f1
+
+        使用传统的递归计算斐波那契数列，需要计算很多重复的值，效率很低，时间复杂度为O(2^n)
+     */
+
+    // 传统的，使用递归的斐波那契数列
+    public static long traditionalFibonacci(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+
+        return traditionalFibonacci(n - 1) + traditionalFibonacci(n - 2);
+    }
+
+    // 使用动态规划计算斐波那契数列
+    public static long fibonacciSequence(int n) {
+        // 构造一个数组存储每一个斐波那契值
+        long[] fiboValue = new long[n];
+        fiboValue[0] = 0;
+        fiboValue[1] = 1;
+        for (int i = 2; i < n; i++) {
+            fiboValue[i] = fiboValue[i - 1] + fiboValue[i - 2];
+        }
+        return fiboValue[n - 1];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+}
