bcb310c539dfd6cb6c4abef512da4e982184662d
Joe Zane <joezane.cn@gmail.com>
2022-07-07 14:53:28 +0800

    1. matrix Trie static optimize

--- src/main/java/com/toocol/lc/TrieMatrix.java
+++ src/main/java/com/toocol/lc/TrieMatrix.java
@@ -1,5 +1,7 @@
 package com.toocol.lc;
 
+import java.util.Arrays;
+
 /**
  * 前缀树/字典树
  * Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
@@ -15,9 +17,17 @@ package com.toocol.lc;
 public class TrieMatrix {
     private static final int LINES = (int)1e6 + 9;
 
-    private final int[][] trie = new int[LINES][26];
-    private final int[] count = new int[LINES];
-    private int index = 0;
+    private static final int[][] trie = new int[LINES][26];
+    private static final int[] count = new int[LINES];
+    private static int index = 0;
+
+    public TrieMatrix() {
+        for (int[] ts : trie) {
+            Arrays.fill(ts, 0);
+        }
+        Arrays.fill(count, 0);
+        index = 0;
+    }
 
     public void insert(String str) {
         int p = 0;
